7a686a921e7ceb19930e380111404eb68b06e9f5
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-03-20 01:07:43 +0100

    Added files for the mobile version

---
+++ app/src/main/java/com/example/feedtrack/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.feedtrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView feedtrack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        feedtrack = findViewById(R.id.feedtrack);
+        feedtrack.getSettings().setJavaScriptEnabled(true);
+        feedtrack.setWebViewClient(new WebViewClient());
+        feedtrack.loadUrl("https://feedtrack.vercel.app/");
+    }
+}
