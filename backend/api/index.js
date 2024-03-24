a25bc740511a7aa120bd67ee5fad41b728f482c4
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-03-24 01:39:03 +0100

    Start implementation of jwt and google sign in

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,18 +1,37 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-
-dotenv.config(); 
-
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-console.log('DB_PORT:', process.env.DB_PORT);
+import mysql from "mysql2";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import jwt from "jsonwebtoken";
 
 const app = express();
+dotenv.config();
 
+const PORT = 3000;
+const URL = process.env.BACKEND_URL || "http://localhost:3000";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      callbackURL: `${URL}/auth/google/callback`,
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile, "profile");
+
+      // TODO: Store user in the database
+
+      done(null, user);
+    }
+  )
+);
+
+app.use(passport.initialize());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const connection = mysql.createConnection({
@@ -20,33 +39,33 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
 });
 
 connection.connect((error) => {
   if (error) {
-    console.error('Error connecting to MySQL database:', error);
+    console.error("Error connecting to MySQL database:", error);
   } else {
-    console.log('Connected to MySQL database!');
+    console.log("Connected to MySQL database!");
   }
 });
 
 app.get("/api", (req, res) => {
-    res.json({ name: "Hana" }); 
+  res.json({ name: "Hana" });
 });
 
 app.get("/api/branches", (req, res) => {
-    connection.query('SELECT * FROM Branch', (error, results) => {
-      if (error) {
-        console.error('Error fetching branches:', error);
-        res.status(500).json({ error: 'Error fetching branches' });
-      } else {
-        console.log('Branches:', results);
-        res.json(results);
-      }
-    });
+  connection.query("SELECT * FROM Branch", (error, results) => {
+    if (error) {
+      console.error("Error fetching branches:", error);
+      res.status(500).json({ error: "Error fetching branches" });
+    } else {
+      console.log("Branches:", results);
+      res.json(results);
+    }
+  });
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+app.listen(PORT || 3000, () =>
+  console.log(`Server ready on port ${PORT || 3000}.`)
+);
