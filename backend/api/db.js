1f3cbf2674dbe3517cabe6abe52a50e635e98e25
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-03-25 23:03:27 +0100

    Backend refactor

---
+++ backend/api/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  multipleStatements: true,
+});
+
+con.connect((error) => {
+  if (error) {
+    console.error("Error connecting to MySQL database:", error);
+  } else {
+    console.log("Connected to MySQL database!");
+  }
+});
+
+module.exports = con;
---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json("You are not authenticated!");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json("Token is not valid!");
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = { authenticateToken };
