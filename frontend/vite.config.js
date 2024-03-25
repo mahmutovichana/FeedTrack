9399f512726dd42bec4a83d742e4a3568f3d780a
msikiric1 <115873702+msikiric1@users.noreply.github.com>
2024-03-25 01:14:07 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/free-brands-svg-icons',
-        '@fortawesome/free-solid-svg-icons'
-      ]
-
-    }
-  }
+  plugins: [react()]
 })
