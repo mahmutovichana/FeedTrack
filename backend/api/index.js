f92305c37aed758cd982dd39a8485e939d2966eb
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-03-26 02:41:48 +0100

    Final changes to swagger

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,7 +4,6 @@ const app = express();
 const cors = require("cors");
 
 const swaggerUI = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 const swaggerJson = require("./swagger.json");
@@ -20,7 +19,7 @@ app.use(
     next();
   },
   swaggerUI.serveFiles(),
-  swaggerUI.setup()
+  swaggerUI.setup(null, { customCssUrl: CSS_URL })
 );
 
 const PORT = process.env.PORT || 3000;
