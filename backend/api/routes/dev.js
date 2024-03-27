d74b4481d631f471a258c2aa2d0ccdd228f91025
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-03-27 02:09:12 +0100

    Finish backend rewrite

--- backend/api/routes/dev.js
+++ backend/api/routes/dev.js
@@ -2,23 +2,23 @@ const router = require("express").Router();
 const db = require("../db");
 
 router.get("/tables", async (req, res) => {
-  const tables = ["Branch", "Feedback", "Person", "Report", "Teller"];
+  const tables = ['"Branch"', '"Feedback"', '"Person"', '"Report"', '"Teller"'];
 
   const sql = tables.map((name) => `SELECT * FROM ${name}`).join(";");
 
   const result = await db.query(sql);
 
-  const html = result.rows
-    .map((table, index) => {
+  const html = result
+    .map(({ rows }, index) => {
       let html = `<h1>${tables[index]}</h1><table border="1">`;
 
       html += `<tr><th>#</th>`;
-      Object.keys(table[0]).forEach((key) => {
+      Object.keys(rows[0]).forEach((key) => {
         html += `<th>${key}</th>`;
       });
       html += "</tr>";
 
-      table.forEach((row, index) => {
+      rows.forEach((row, index) => {
         html += `<tr><td>${index + 1}</td>`;
         Object.values(row).forEach((item) => {
           html += `<td>${item}</td>`;
