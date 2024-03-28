43c9932486c9f67532c72bdd05aec0150533522c
Harun Alagic <halagic1.etf.unsa.ba>
2024-03-28 02:09:20 +0100

    Small fix

--- frontend/src/views/AdminPanel/AdminPanelHomePageView.jsx
+++ frontend/src/views/AdminPanel/AdminPanelHomePageView.jsx
@@ -30,7 +30,7 @@ const AdminHomePage = () => {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({"refreshToken": localStorage.getItem("token")})
+                body: JSON.stringify({"refreshToken": localStorage.getItem("refreshToken")})
             });
 
             if (response.ok) {
