bb2f3ce06c6368ac54016408072217a0f797f180
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-03-29 16:51:28 +0100

    changed link

--- frontend/src/views/AdminPanel/AdminPanelHomePageView.jsx
+++ frontend/src/views/AdminPanel/AdminPanelHomePageView.jsx
@@ -24,7 +24,7 @@ const AdminHomePage = () => {
         event.preventDefault();
         console.log(JSON.stringify({"token": localStorage.getItem("token")}));
         try {
-            const response = await fetch('https://feedtrack-backend.vercel.appp/api/logout', {
+            const response = await fetch('https://feedtrack-backend.vercel.app/api/logout', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
