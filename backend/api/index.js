8dd225d3f53882f068e135ab4198c41f50573252
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-06 15:47:22 +0200

    config files

--- backend/api/index.js
+++ backend/api/index.js
@@ -29,19 +29,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 
-/*
+
 app.use(cors({
     origin: 'https://feed-track-backup.vercel.app/' ,
     credentials: true
 
 }));
-*/
+/*
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});
+});*/
 
 app.use("/api", crudRouter);
 app.use("/api", authRouter);
