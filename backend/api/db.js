4352185a7e7bb17aa7ad38cf9ae9d2787ae64254
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-06 18:18:39 +0200

    change of localhost

--- backend/api/db.js
+++ backend/api/db.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const { Pool } = require("pg");
 
-console.log('DB_CONN_STRING:', process.env.DB_CONN_STRING);
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
 
 const pool = new Pool({
-  connectionString: process.env.DB_CONN_STRING,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD,
 });
 
 pool.connect((err) => {
@@ -13,11 +17,7 @@ pool.connect((err) => {
     return;
   }
 
-  if (process.env.DB_CONN_STRING.includes('localhost')) {
-    console.log("Connected to database locally!");
-  } else {
-    console.log("Connected to database on the internet!");
-  }
+  console.log("Connected to the database!");
 });
 
 module.exports = {
