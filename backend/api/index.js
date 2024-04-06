4e9001442f72346f36f52c8e68aac2a6d41978c1
Harun Alagic <halagic1.etf.unsa.ba>
2024-04-06 14:04:42 +0200

    Changed cors again

--- backend/api/index.js
+++ backend/api/index.js
@@ -28,11 +28,18 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
-
+/*
 app.use(cors({
     origin: ['https://feed-track-backup.vercel.app/' , 'https://feed-track-backend.vercel.app/' ],
     credentials: true
-}));
+
+}));*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use("/api", crudRouter);
 app.use("/api", authRouter);
