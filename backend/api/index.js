88b7e9b8dc9e27f90e4e3377468064744fa6943c
Harun Alagic <halagic1.etf.unsa.ba>
2024-04-06 15:18:42 +0200

    still db problems

--- backend/api/index.js
+++ backend/api/index.js
@@ -29,16 +29,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 
+/*
 app.use(cors({
-    origin: 'https://feed-track-backup.vercel.app' ,
+    origin: 'https://feed-track-backup.vercel.app/' ,
     credentials: true
+
 }));
-/*app.use((req, res, next) => {
+*/
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});*/
+});
 
 app.use("/api", crudRouter);
 app.use("/api", authRouter);
