d6edfa3ab17e83b734d85b080901ae9d811fa0e4
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-06 16:28:00 +0200

    changed config

--- backend/api/index.js
+++ backend/api/index.js
@@ -29,9 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 
-
 app.use(cors({
-  origin: "*"
+  origin: "https://feed-track-backup.vercel.app"
 }));
 
 app.use((req, res, next) => {
