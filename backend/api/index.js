8660d5bfc4dbad280611255d1cb7231a7a1b2e85
Harun Alagic <halagic1.etf.unsa.ba>
2024-04-06 15:14:00 +0200

    one last try

--- backend/api/index.js
+++ backend/api/index.js
@@ -34,12 +34,12 @@ app.use(cors({
     credentials: true
 
 }));
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});
+});*/
 
 app.use("/api", crudRouter);
 app.use("/api", authRouter);
