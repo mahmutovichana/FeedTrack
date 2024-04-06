b30d28ef1848c600c49ef5bc395c076cb8ce6c11
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-06 16:48:24 +0200

    changes to cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -29,15 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 
-app.use(cors({
-  origin: "https://feed-track-backup.vercel.app"
-}));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://feed-track-backup.vercel.app');
-  next();
-});
-
+app.use(cors());
 
 /*
 app.use((req, res, next) => {
