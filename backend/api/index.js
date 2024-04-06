9ee0bf0f09fadc9bcd03bfcba3fea74f37805a77
Harun Alagic <halagic1.etf.unsa.ba>
2024-04-06 14:10:17 +0200

    added vercel plus index fix

--- backend/api/index.js
+++ backend/api/index.js
@@ -35,9 +35,9 @@ app.use(cors({
 
 }));*/
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
