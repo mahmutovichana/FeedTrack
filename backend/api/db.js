85425a91d251570d351b370d3e64ef73b1950a5a
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-06 18:24:07 +0200

    direct conn

--- backend/api/db.js
+++ backend/api/db.js
@@ -4,12 +4,18 @@ const { Pool } = require("pg");
 const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
 
 const pool = new Pool({
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_DATABASE,
-  user: DB_USER,
-  password: DB_PASSWORD,
+  host: "aws-0-eu-central-1.pooler.supabase.com",
+  port: 5432,
+  database: "postgres",
+  user: "postgres.gavgivgrmhhepowbdlxb",
+  password: "feedtrack_password",
 });
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
 
 pool.connect((err) => {
   if (err) {
