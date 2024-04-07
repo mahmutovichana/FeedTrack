58fe2f88496976a36f4025d12e7940eb9a17324d
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-07 15:56:43 +0200

    resolved conflict

--- backend/api/index.js
+++ backend/api/index.js
@@ -27,11 +27,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-
-app.use(cors({
-    origin: 'https://feedtrack.vercel.app',
-  }));
+app.use(cors());
 
 /*
 app.use((req, res, next) => {
