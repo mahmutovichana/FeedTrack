0eb578837b9bd111ee511b01a71c98bedcae070f
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-04-09 19:02:25 +0200

    changed deploy urls

--- backend/api/constants.js
+++ backend/api/constants.js
@@ -13,10 +13,10 @@ const swagger = {
 };
 
 const deployURLs = {
-  // backendURL: "https://feedtrack-backend.vercel.app",
-  // frontendURL: "https://feedtrack.vercel.app"
-  backendURL: "http://localhost:5432",
-  frontendURL: "http://localhost:5173"
+  backendURL: "https://feedtrack-backend.vercel.app",
+  frontendURL: "https://feedtrack.vercel.app"
+  // backendURL: "http://localhost:5432",
+  // frontendURL: "http://localhost:5173"
 }
 
 module.exports = {
