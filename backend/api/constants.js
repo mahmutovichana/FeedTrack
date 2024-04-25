8deee01f7a157dd8bdf4f604d15652c46d0368ad
HBosno <hbosno1@etf.unsa.ba>
2024-04-25 18:07:27 +0200

    change deploy urls

--- backend/api/constants.js
+++ backend/api/constants.js
@@ -13,8 +13,8 @@ const swagger = {
 };
 
 const deployURLs = {
-  backendURL: " http://localhost:5432", // http://localhost:5432
-  frontendURL: "http://localhost:5173" // http://localhost:5173
+  backendURL: " https://feedtrack-backend.vercel.app", // http://localhost:5432
+  frontendURL: "https://feedtrack.vercel.app" // http://localhost:5173
 }
 
 module.exports = {
--- frontend/public/constants.js
+++ frontend/public/constants.js
@@ -1,11 +1,4 @@
 export const deployURLs = {
-  backendURL: "http://localhost:5432",
-  frontendURL: "http://localhost:5173"
-}
-
-/*
-export const deployURLs = {
-  backendURL: "https://feedtrack-backend.vercel.app",
-  frontendURL: "https://feedtrack.vercel.app"
-}
-*/
+  backendURL: "https://feedtrack-backend.vercel.app", // "http://localhost:5432",
+  frontendURL: "https://feedtrack.vercel.app", // "http://localhost:5173" // "https://feedtrack.vercel.app"
+};
