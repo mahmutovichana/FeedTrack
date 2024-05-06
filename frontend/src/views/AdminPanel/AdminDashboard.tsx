34527106835f0730cc4b69de50719fae9818ac75
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2024-05-07 00:05:55 +0200

    added pdf download

--- frontend/src/views/AdminPanel/AdminDashboard.tsx
+++ frontend/src/views/AdminPanel/AdminDashboard.tsx
@@ -21,37 +21,11 @@ const AdminDashboardPage = () => {
   useEffect(() => {
     if (localStorage.getItem("token") == null) navigate("/");
   });
+
   return (
     <div className="home">
-
-      <iframe id="lookerDashboard" src="https://lookerstudio.google.com/embed/reporting/0d38b52c-a24b-4bce-aed1-c5c65c9a9a54/page/qlD" frameBorder="0" allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
-      {/* <div className="box box1">
-        <TopBox />
-      </div>
-      <div className="box box2">
-        <ChartBox {...chartBoxUser} />
-      </div>
-      <div className="box box3">
-        <ChartBox {...chartBoxProduct} />
-      </div>
-      <div className="box box4">
-        <PieChartBox />
-      </div>
-      <div className="box box5">
-        <ChartBox {...chartBoxConversion} />
-      </div>
-      <div className="box box6">
-        <ChartBox {...chartBoxRevenue} />
-      </div>
-      <div className="box box7">
-        <BigChartBox />
-      </div>
-      <div className="box box8">
-        <BarChartBox {...barChartBoxVisit} />
-      </div>
-      <div className="box box9">
-        <BarChartBox {...barChartBoxRevenue} />
-      </div> */}
+      <p>If you want to download this dashboard as a PDF, right click on it 😊</p>
+      <iframe id="lookerDashboard" src="https://lookerstudio.google.com/embed/reporting/0d38b52c-a24b-4bce-aed1-c5c65c9a9a54/page/qlD" frameBorder="0" allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-downloads"></iframe>
     </div>
   );
 };
